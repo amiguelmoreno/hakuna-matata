ab6040b528b517e68086f9b0822a2e7bbbcc5ab8
Miguel Moreno <amiguelmoreno42@gmail.com>
2022-12-19 19:46:43 +0100

    add map location

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,7 +2,8 @@ function Map() {
     return (
         <div id="location" className="map">
             <h2>¿Dónde estamos?</h2>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12791.897775757594!2d-4.4431154!3d36.723175!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb09534f3a4e9c985!2sHM%20%7C%20Peluquer%C3%ADa%20Canina!5e0!3m2!1ses!2ses!4v1671473158969!5m2!1ses!2ses"></iframe>
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25582.786376193544!2d-4.474149198420028!3d36.726204467670286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd72f719ab3101b3%3A0x9f9c5a36deb2a159!2sHakuna%20Matata%20Peluquer%C3%ADa%20Canina!5e0!3m2!1ses!2ses!4v1671474530719!5m2!1ses!2ses"></iframe>
         </div>
     );
 }
