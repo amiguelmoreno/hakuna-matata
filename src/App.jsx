555ed3a6c788aa4a7a1f719f372b2db4f4ad998d
Miguel Moreno <amiguelmoreno42@gmail.com>
2022-12-20 17:04:40 +0100

    add package router

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Feedbacks from "./components/Feedbacks/Feedbacks";
 import Services from "./components/Services/Services";
 import Map from "./components/Map/Map";
 import WhatsappBtn from "./components/WhatsappBtn/WhatsappBtn";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
     return (
-        <div className="App">
-            <Header></Header>
-            <Nav></Nav>
-            <Services></Services>
-            <Feedbacks></Feedbacks>
-            <Map></Map>
-            <WhatsappBtn></WhatsappBtn>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Header></Header>
+                <Nav></Nav>
+                <Services></Services>
+                <Feedbacks></Feedbacks>
+                <Map></Map>
+                <WhatsappBtn></WhatsappBtn>
+            </div>
+        </BrowserRouter>
     );
 }
 
--- src/components/Feedbacks/Feedbacks.jsx
+++ src/components/Feedbacks/Feedbacks.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import FeedbackSlider from "./FeedbackSlider";
 
 const feedbacks = [
     {
